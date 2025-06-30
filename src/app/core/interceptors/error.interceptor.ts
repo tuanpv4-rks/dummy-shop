da81a0f0567fa43a3511690f8b66a1618fc0c073
tuanpv4 <tuanpv4@rikkeisoft.com>
2025-06-30 19:47:26 +0700

    chore: clean code

--- src/app/core/interceptors/error.interceptor.ts
+++
@@ -1,6 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
-
-export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req).pipe(catchError((err) => throwError(() => err.error)));
-};
--- src/app/features/product/store/product.state.spec.ts
+++ src/app/features/product/store/product.state.spec.ts
@@ -1,26 +1,98 @@
 import { TestBed } from '@angular/core/testing';
-import {  provideStore,  Store } from '@ngxs/store';
-import { ProductState, ProductStateModel } from './product.state';
-import { ProductAction } from './product.actions';
+import { NgxsModule, Store } from '@ngxs/store';
+import { of } from 'rxjs';
 
-describe('Product store', () => {
+import { ProductService } from '../services/product.service';
+import { ISimpleProduct } from '../models/product.model';
+
+import { ProductState, ProductStateModel } from './product.state';
+import {
+  LoadProducts,
+  AddToFavorites,
+  RemoveFromFavorites,
+} from './product.actions';
+
+describe('ProductState', () => {
   let store: Store;
+  let productService: jasmine.SpyObj<ProductService>;
+
+  const mockProducts: ISimpleProduct[] = [
+    { id: 1, title: 'Product 1', price: 10, description: 'Desc 1', thumbnail: 'thumb1.jpg' },
+    { id: 2, title: 'Product 2', price: 20, description: 'Desc 2', thumbnail: 'thumb2.jpg' },
+  ];
+
   beforeEach(() => {
+    const productServiceSpy = jasmine.createSpyObj('ProductService', ['getProducts']);
+
     TestBed.configureTestingModule({
-       providers: [provideStore([ProductState])]
-      
+      imports: [NgxsModule.forRoot([ProductState])],
+      providers: [{ provide: ProductService, useValue: productServiceSpy }],
     });
 
     store = TestBed.inject(Store);
+    productService = TestBed.inject(ProductService) as jasmine.SpyObj<ProductService>;
   });
 
-  it('should create an action and add an item', () => {
-    const expected: ProductStateModel = {
-      items: ['item-1']
+  it('should have correct initial state', () => {
+    const state: ProductStateModel = store.selectSnapshot(s => s.product);
+    expect(state.products).toEqual([]);
+    expect(state.favoriteIds).toEqual([]);
+    expect(state.isLoading).toBe(false);
+  });
+
+  describe('Actions', () => {
+    it('should dispatch LoadProducts action and update state', () => {
+      productService.getProducts.and.returnValue(of(mockProducts));
+
+      store.dispatch(new LoadProducts());
+
+      const state: ProductStateModel = store.selectSnapshot(s => s.product);
+      expect(productService.getProducts).toHaveBeenCalled();
+      expect(state.products).toEqual(mockProducts);
+    });
+
+    it('should dispatch AddToFavorites action and update favoriteIds', () => {
+      store.reset({ product: { products: [], favoriteIds: [], isLoading: false } });
+      store.dispatch(new AddToFavorites(1));
+
+      const favoriteIds = store.selectSnapshot(ProductState.getFavoriteIds);
+      expect(favoriteIds).toEqual([1]);
+    });
+
+    it('should dispatch RemoveFromFavorites action and update favoriteIds', () => {
+      store.reset({ product: { products: [], favoriteIds: [1, 2], isLoading: false } });
+      store.dispatch(new RemoveFromFavorites(1));
+
+      const favoriteIds = store.selectSnapshot(ProductState.getFavoriteIds);
+      expect(favoriteIds).toEqual([2]);
+    });
+  });
+
+  describe('Selectors', () => {
+    const fullState = {
+      product: {
+        products: mockProducts,
+        favoriteIds: [1],
+        isLoading: false,
+      },
     };
-    store.dispatch(new ProductAction('item-1'));
-    const actual = store.selectSnapshot(ProductState.getState);
-    expect(actual).toEqual(expected);
-  });
 
+    it('should return correct values from getProducts selector', () => {
+      store.reset(fullState);
+      const products = store.selectSnapshot(ProductState.getProducts);
+      expect(products).toEqual(mockProducts);
+    });
+
+    it('should return correct values from getFavorites selector', () => {
+      store.reset(fullState);
+      const favorites = store.selectSnapshot(ProductState.getFavorites);
+      expect(favorites).toEqual([mockProducts[0]]);
+    });
+
+    it('should return correct values from getFavoriteIds selector', () => {
+      store.reset(fullState);
+      const favoriteIds = store.selectSnapshot(ProductState.getFavoriteIds);
+      expect(favoriteIds).toEqual([1]);
+    });
+  });
 });
